0c987f88f201707259918f9fb73f312c90ab6b1e
Muhamad Syafii <muhamadsyafii4@gmail.com>
2022-03-08 11:01:27 +0700

    add new layout with item , add some packkage

--- app/src/main/java/com/example/restaurantreview/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/restaurantreview/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.restaurantreview
+package com.example.restaurantreview.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.restaurantreview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
